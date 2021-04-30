cdf2cba614737e9da1922ad4c7321e3c72c2f28b
Gaston <gaston.rai28@gmail.com>
2021-04-30 20:30:04 -0300

    Initialize Project

---
+++ database/database.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS project_login;
+USE project_login;
+
+CREATE TABLE users(
+id          int() auto_increment not null,
+role        varchar(50) not null,
+name        varchar(100) not null,
+surname     varchar(100) not null,
+nick        varchar(100) not null,
+email       varchar(200) not null,
+password    varchar(200) not null,
+image       varchar(255) not null,
+created_at  datetime default CURRENT_TIMESTAMP(),
+update_at   datetime default CURRENT_TIMESTAMP(),
+token       text(),
+CONSTRAINT pk_users PRIMARY KEY(id)
+)ENGINE=Innodb DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE images(
+    id              int() auto_increment not null,
+    user_id         int() not null,
+    path            text() not null,
+    description     varchar(255) not null,
+    created_at      datetime default CURRENT_TIMESTAMP(),
+    update_at       datetime default CURRENT_TIMESTAMP(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)ENGINE=Innodb DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
